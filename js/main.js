2c75ad8d07eb0a9b3e81a2d513cbdde7c95764e2
Kevin Hodges <khodges42@gmail.com>
2018-07-09 23:44:22 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -434,14 +434,18 @@ var main = (function () {
 
     TypeSimulator.prototype.type = function (text, callback) {
         var isURL = (function () {
-            try{
-            return function (str) {
-                return (str.startsWith("http") || str.startsWith("www")) && str.indexOf(" ") === -1 && str.indexOf("\n") === -1;
+            return function (str="") {
+                    var returnState = false;
+                    try { 
+                        returnState = (str.startsWith("http") || str.startsWith("www")) && str.indexOf(" ") === -1 && str.indexOf("\n") === -1; 
+                    } 
+                    catch {
+                        returnState = false;
+                    }
+                    finally { 
+                        return returnState; 
+                    } 
             };
-            }
-            catch(error){
-                console.log(error);
-                return False;
             }
         })();
         if (isURL(text)) {
